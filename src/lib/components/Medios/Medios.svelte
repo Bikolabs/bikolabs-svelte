610fc06bb80240d9d1706a36b5197f33cfb3abe8
Aitor Resano <aitor.resano@biko2.com>
2023-06-15 12:52:21 +0200

    Links correctos

--- src/lib/components/Medios/Medios.svelte
+++ src/lib/components/Medios/Medios.svelte
@@ -8,8 +8,8 @@
     <p class="">Entrevistas en Radio y Tv</p>
   </div>
   <div class="flex flex-row flex-wrap mt-10">
-    <Item title="Gen Playz " description="Participación de Ujué y Karlos en el programa de RTVE sobre IA" logo="/images/web/logos/playz.png" link="https://joseluisserrano.net/podcast/inteligencia-artificial-toma-decisiones/" />
-    <Item title="eduHacking Podcast" description="Participación de Bikolabs en el podcast eduHacking" logo="/images/web/logos/eduhacking.png" link="/" />
+    <Item title="Gen Playz " description="Participación de Ujué y Karlos en el programa de RTVE sobre IA" logo="/images/web/logos/playz.png" link="https://www.rtve.es/play/videos/gen-playz-xl/inteligencia-artificial/6839310/" />
+    <Item title="eduHacking Podcast" description="Participación de Bikolabs en el podcast eduHacking" logo="/images/web/logos/eduhacking.png" link="https://joseluisserrano.net/podcast/inteligencia-artificial-toma-decisiones/" />
     <Item title="AlgorithmWatch." description="Mención a nuestro de investigación sobre sistemas predictivos" logo="/images/web/logos/aw-logo.svg" link="https://algorithmwatch.org/en/basque-country-gender-violence-algorithm/" />
     <Item title="Xataka artículo" description="Nuestra intervención Sebastian, el asistente que controla al asistente" logo="/images/web/logos/xataka.svg" link="https://www.xataka.com/accesorios/asistente-que-controla-al-asistente-este-interruptor-voz-genial-invento-para-decidir-cuando-queremos-ser-escuchados" />
     <Item title="Zoom.net" description="Entrevista a Bikolabs sobre IA, sesgos y predicción" logo="/images/web/logos/rtve.svg" link="https://www.rtve.es/play/videos/zoom-net/irisbond-mascarilla-lg-bikolabs/5776985/" />
